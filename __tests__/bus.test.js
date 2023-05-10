cf86bf3aa28000e52c7671af44931ca815e55e14
Lakshmi-kande <kandelakshmi500@gmail.com>
2023-05-10 21:21:07 +0530

    unit testing on busDetails

--- __tests__/bus.test.js
+++ __tests__/bus.test.js
@@ -1,156 +1,117 @@
 const request = require('supertest');
 const app = require('../index');
-const mongoose = require('mongoose');
 const busDetails = require('../model/bus');
-require('dotenv').config();
+const { constants } = require('../constants');
 
-let db;
+jest.mock('../model/bus');
 
-beforeAll(async () => {
-  db = await mongoose.connect(process.env.connect_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  });
-});
-
-afterAll(async () => {
-  await db.disconnect();
-  await mongoose.connection.close();
-});
-
-
-describe('GET /api/buses', () => {
-  it('should get all buses list', async () => {
-    const res = await request(app).get('/api/buses');
-    expect(res.statusCode).toBe(200);
-    expect(Array.isArray(res.body)).toBeTruthy();
-  }, 10000);
-});
-
-
-
-describe('POST /api/buses', () => {
+describe('Bus API', () => {
+  let busData = {
+    busNum: '1234',
+    busType: 'AC',
+    startCity: 'Mumbai',
+    destination: 'Pune',
+    totalSeats: 50,
+    availableSeats: 50,
+  };
   
-  it('should create a new bus details', async () => {
-    const res = await request(app)
-      .post('/api/buses')
-      .send({
-        busNum: '1234',
-        busType: 'AC',
-        startCity: 'Mumbai',
-        destination: 'Pune',
-        totalSeats: '50',
-        availableSeats: '50'
-      })
-      expect(res.statusCode).toBe(201);
-
-    expect(res.body).toHaveProperty('_id');
-    expect(res.body.busNum).toBe('1234');
-    expect(res.body.busType).toBe('AC');
-    expect(res.body.startCity).toBe('Mumbai');
-    expect(res.body.destination).toBe('Pune');
-    expect(res.body.totalSeats).toBe('50');
-    expect(res.body.availableSeats).toBe('50');
-  },10000);
-
-  it('should return validation error if required fields are missing', async () => {
-    const res = await request(app)
-      .post('/api/buses')
-      .send({})
-      .expect(400);
-
-    expect(res.body).toEqual({ message: 'All fields are required' });
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
-});
 
+  describe('GET /api/buses', () => {
+    it('should return all buses', async () => {
+      const mockedBusDetails = jest.spyOn(busDetails, 'find');
+      mockedBusDetails.mockResolvedValue([busData]);
 
-describe('GET /api/buses/:id', () => {
-  it('should get a specific bus with the provided Id ', async () => {
-    const bus = new busDetails({
-      busNum: '4321',
-      busType: 'AC',
-      startCity: 'Mumbai',
-      destination: 'Pune',
-      totalSeats: '50',
-      availableSeats: '50'
+      const response = await request(app).get('/api/buses');
+      
+      expect(response.status).toBe(constants.SUCCESSFULL_REQUEST);
+      expect(response.body).toEqual([busData]);
+      expect(mockedBusDetails).toHaveBeenCalledTimes(1);
     });
-    await bus.save();
-
-    const res = await request(app)
-      .get(`/api/buses/${bus._id}`)
-      .send({
-        busNum: '4321',
-        busType: 'AC',
-        startCity: 'Mumbai',
-        destination: 'Pune',
-        totalSeats: '50',
-        availableSeats: '50'
-      });
-
-    expect(res.statusCode).toBe(200);
-    expect(res.body.busNum).toBe('4321');
-    expect(res.body.busType).toBe('AC');
   });
-});
 
-
-describe('PUT /api/buses/:id', () => {
-  it('should update a specific bus with the provided Id', async () => {
-    const bus = new busDetails({
-      busNum: '1234',
-      busType: 'Non-AC',
-      startCity: 'Mumbai',
-      destination: 'Pune',
-      totalSeats: '50',
-      availableSeats: '50'
-    });
-    await bus.save();
-
-    const res = await request(app)
-      .put(`/api/buses/${bus._id}`)
-      .send({
+  
+  describe('POST /api/buses', () => {
+    it('should create a new bus', async () => {
+      const busData = {
         busNum: '4321',
-        busType: 'AC',
-        startCity: 'Mumbai',
+        busType: 'A C',
+        startCity: 'HYD',
         destination: 'Pune',
-        totalSeats: '50',
-        availableSeats: '50'
-      });
+        totalSeats: 50,
+        availableSeats: 50,
+      };
+      const mockedBusSave = jest.spyOn(busDetails.prototype, 'save');
+      mockedBusSave.mockResolvedValue(busData);
 
-    expect(res.statusCode).toBe(200);
-    expect(res.body.busNum).toBe('4321');
-    expect(res.body.busType).toBe('AC');
+      const response = await request(busData)
+        .post('/api/buses')
+        .send(busData);
+
+      expect(response.status).toBe(constants.SUCCESSFULL_POST);
+      expect(response.body).toEqual(busData);
+      expect(mockedBusSave).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return validation error when required fields are missing', async () => {
+      const response = await request(app)
+        .post('/api/buses')
+        .send({});
+
+      expect(response.status).toBe(constants.VALIDATION_ERROR);
+      expect(response.body).toEqual({ message: "All fields are required" });
+    });
   });
-});
 
 
 
-describe('DELETE /api/buses/:id', () => {
-  it('should delete a bus with specific bus with the provided id', async () => {
-    const bus = new busDetails({
-      busNum: '1234',
-      busType: 'Non-AC',
-      startCity: 'Mumbai',
-      destination: 'Pune',
-      totalSeats: '50',
-      availableSeats: '50'
+
+  describe('GET /api/buses/:id', () => {
+    it('should get a specific bus with the provided Id ', async () => {
+      const mockedBusDetails = jest.spyOn(busDetails, 'findById');
+      mockedBusDetails.mockResolvedValue(busData);
+
+      const response = await request(app).get('/api/buses/1');
+      
+      expect(response.status).toBe(constants.SUCCESSFULL_REQUEST);
+      expect(response.body).toEqual(busData);
+      expect(mockedBusDetails).toHaveBeenCalledWith('1');
+      expect(mockedBusDetails).toHaveBeenCalledTimes(1);
     });
-    await bus.save();
+  });
 
-    const res = await request(app)
-      .delete(`/api/buses/${bus._id}`)
-      .send({
-        busNum: '4321',
-        busType: 'AC',
-        startCity: 'Mumbai',
-        destination: 'Pune',
-        totalSeats: '50',
-        availableSeats: '50'
-      });
 
-      expect(res.statusCode).toBe(200);
-      expect(res.body.message).toBe('deleted successfully');
-  },10000);
+  describe('PUT /api/buses/:id', () => {
+    it('should update a specific bus with the provided Id', async () => {
+      const mockedBusDetails = jest.spyOn(busDetails, 'findByIdAndUpdate');
+      mockedBusDetails.mockResolvedValue(busData);
+
+      const response = await request(app)
+        .put('/api/buses/1')
+        .send(busData);
+
+      expect(response.status).toBe(constants.SUCCESSFULL_REQUEST);
+      expect(response.body).toEqual(busData);
+      expect(mockedBusDetails).toHaveBeenCalledWith('1', busData, { new: true });
+      expect(mockedBusDetails).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('DELETE /api/buses/:id', () => {
+    it('should delete a specific bus with the provided Id', async () => {
+      const mockedBusDetails = jest.spyOn(busDetails, 'findByIdAndDelete');
+      mockedBusDetails.mockResolvedValue(busData);
+
+      const response = await request(app).delete('/api/buses/1');
+
+      expect(response.status).toBe(constants.SUCCESSFULL_REQUEST);
+      expect(response.body).toEqual(busData);
+      expect(mockedBusDetails).toHaveBeenCalledWith('1');
+      expect(mockedBusDetails).toHaveBeenCalledTimes(1);
+    });
+  });
+
 });
+
